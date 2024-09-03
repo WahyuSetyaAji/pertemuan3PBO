9b325bf93d5de345083949a58a853d54be85c730
WahyuSetyaAji <wahyusetyaaji10@gmail.com>
2024-09-03 13:54:03 +0700

    first commit

---
+++ DemoNilai.java
@@ -0,0 +1,9 @@
+package Pertemuan3;
+
+public class DemoNilai {
+    public static void main(String[] args) {
+        Nilai nilaiMahasiswa = new Nilai("2310631170119", "Wahyu Setya Aji", 100, 85, 85, 90);
+        
+        nilaiMahasiswa.CetakNilai();
+    }
+}
---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,11 @@
+package Pertemuan3;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        
+        mahasiswa.membaca();
+        mahasiswa.nyontek();
+        mahasiswa.modifikasi();
+    }
+}
